324c6bd2e814f4360fbb834e3e93c3eac56659f5
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-02-10 17:10:33 -0300

    SortedArrayToBST
    SortedArrayToBST

--- LeetCode.cs
+++ LeetCode.cs
@@ -382,4 +382,21 @@ public class Solution {
         }
         return correcto && (pila.Count == 0);
     }
+    
+    public TreeNode createBST(int[] nums, int start, int end){
+        if(start > end){
+            return null;
+        }
+        int mid = start + (end-start)/2;
+        TreeNode node = new TreeNode(nums[mid]);
+        
+        node.left = createBST(nums, start, mid - 1);
+        node.right = createBST(nums, mid + 1, end);
+        
+        return node;
+    }
+
+    public TreeNode SortedArrayToBST(int[] nums) {
+        return createBST(nums, 0, nums.Length - 1);
+    }
 }
